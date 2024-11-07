dd4e02673a480a03dd50acaf01f102c9bf15862e
Abadat Hossain <abadat26@gmail.com>
2024-11-07 10:35:06 +0600

    problem 5 done

---
+++ problem-5.ts
@@ -0,0 +1,14 @@
+{
+  // 
+
+  function getProperty<T>(obj: T, key: keyof T): T[keyof T] {
+    console.log(key)
+    return obj[key];
+  }
+
+  const person = { name: "Alice", age: 30 };
+  console.log(getProperty(person, "name"));
+
+
+  // 
+}
